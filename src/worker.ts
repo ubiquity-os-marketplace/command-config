173dd9a445e2c2f6eff3f4783554497464c2d95a
gentlementlegen <fernand.veyrier@epitech.eu>
2025-06-11 22:09:14 +0300

    chore: fixed knip

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,12 @@
-import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
+import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import { ExecutionContext } from "hono";
 import manifest from "../manifest.json";
-import { runPlugin } from "./index";
-import { Env, envSchema, PluginSettings, pluginSettingsSchema, SupportedEvents } from "./types/index";
-import { Command } from "./types/command";
 import { createAdapters } from "./adapters/index";
+import { runPlugin } from "./index";
+import { Command } from "./types/command";
+import { Env, envSchema, PluginSettings, pluginSettingsSchema, SupportedEvents } from "./types/index";
 
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
