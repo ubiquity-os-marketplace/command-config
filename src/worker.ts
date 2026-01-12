2a2bc616cd5dbf18a8c98c35f0bd844a50826e74
gentlementlegen <fernand.veyrier@epitech.eu>
2026-01-12 16:29:59 +0900

    chore: fixed ncc

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import { createPlugin } from "@ubiquity-os/plugin-sdk";
+import { createPlugin, Options } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import manifest from "../manifest.json";
@@ -18,9 +18,9 @@ export default {
       },
       manifest as Manifest,
       {
-        envSchema,
+        settingsSchema: pluginSettingsSchema as unknown as Options["settingsSchema"],
+        envSchema: envSchema as unknown as Options["envSchema"],
         postCommentOnError: true,
-        settingsSchema: pluginSettingsSchema,
         logLevel: (env.LOG_LEVEL as LogLevel) || LOG_LEVEL.INFO,
         kernelPublicKey: env.KERNEL_PUBLIC_KEY,
         bypassSignatureVerification: true,
