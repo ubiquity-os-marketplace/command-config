948b676939449d043d8276472525129d916c8ded
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 15:52:20 +0900

    fix: align manifest workflow and command schema metadata

--- src/types/command.ts
+++ src/types/command.ts
@@ -2,7 +2,7 @@ import { Type as T } from "@sinclair/typebox";
 import { StaticDecode } from "@sinclair/typebox";
 
 export const commandSchema = T.Object({
-  name: T.Literal("config"),
+  name: T.Literal("config", { description: 'Configure the UbiquityOS plugin.', examples: ['/config <editor_instruction>'] }),
   parameters: T.Object({
     editor_instruction: T.Optional(T.String()),
     editorInstruction: T.Optional(T.String()),
