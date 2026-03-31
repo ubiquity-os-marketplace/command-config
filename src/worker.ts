ef2a0b25cc7d923d59173ff63461b8c0ed03b084
gentlementlegen <fernand.veyrier@epitech.eu>
2026-03-31 17:51:00 +0900

    chore: with assertion json import

--- src/worker.ts
+++ src/worker.ts
@@ -1,7 +1,7 @@
 import { createPlugin, Options } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import manifest from "../manifest.json";
+import manifest from "../manifest.json" with { type: "json" };
 import { createAdapters } from "./adapters/index";
 import { runPlugin } from "./index";
 import { Command } from "./types/command";
